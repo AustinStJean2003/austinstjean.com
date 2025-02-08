50b3e4355a2acfba6b9527f0518b88feb6763a7b
2034907 <2034907@cegep-heritage.qc.ca>
2025-02-07 19:20:10 -0500

    Added networks tab and restructured files.

---
+++ scripts/networks.js
@@ -0,0 +1,55 @@
+fetch('/scripts/networks.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('networks-container');
+
+        // Loop through each object in the JSON array
+        data.forEach(item => {
+
+            const linkItem = document.createElement('a')
+            linkItem.classList.add("linkItem")
+            linkItem.target = "_blank"
+            linkItem.href = item.link;
+
+            const networkItem = document.createElement('div');
+            networkItem.classList.add('network-item');
+
+            const img = document.createElement('img');
+            img.src = item.image;
+            img.alt = item.name;
+            img.title = item.name;
+
+            const username = document.createElement('div');
+            username.classList.add('username');
+            username.innerText = item.username;
+
+            linkItem.appendChild(networkItem);
+            networkItem.appendChild(img);
+            networkItem.appendChild(username);
+
+            container.appendChild(linkItem);
+
+            // const networkItem = document.createElement('div');
+            // networkItem.classList.add('network-item');
+
+            // const linkItem = document.createElement('a')
+            // linkItem.classList.add("linkItem")
+            // linkItem.href = item.link;
+
+            // const img = document.createElement('img');
+            // img.src = item.image;
+            // img.alt = item.name;
+            // img.title = item.name;
+
+            // const username = document.createElement('div');
+            // username.classList.add('username');
+            // username.innerText = item.username;
+
+            // networkItem.appendChild(linkItem);
+            // linkItem.appendChild(img);
+            // linkItem.appendChild(username);
+
+            // container.appendChild(networkItem);
+        });
+    })
+    .catch(error => console.error('Error loading JSON data:', error));
